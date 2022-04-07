6d840fd67aff618c53b9262b8e3bd9e62bea44fb
Wayne <waiting0819@msn.com>
2022-04-07 15:50:57 +0800

    fix typo

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -9,7 +9,7 @@ function Hero() {
         </div>
         <div className="content-right leading-6">
           <div className="text-3xl font-bold mb-2">Hi 👋</div>
-          I&apos;m Waiting7777, a web developer specializing in <b>modern Javascript</b>, <b>Data Visulization</b>, <b>Crypto</b>.<br/><br/>I&apos;ve been building stuff on the web for the last 7 years, working with clients across various countries and industries.<br/><br/>This website is my digital garden of notes on Backend, frontend, and devops!
+          I&apos;m Waiting7777, a web developer specializing in <b>modern Javascript</b>, <b>Data Visualization</b>, <b>Crypto</b>.<br/><br/>I&apos;ve been building stuff on the web for the last 7 years, working with clients across various countries and industries.<br/><br/>This website is my digital garden of notes on Backend, frontend, and devops!
         </div>
       </div>
     </div>
